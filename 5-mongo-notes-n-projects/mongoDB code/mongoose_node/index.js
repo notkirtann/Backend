9dc1911ea80ec0debf632b124ab06f50dab1b3df
kirtannn1 <119801478+notkirtann@users.noreply.github.com>
2026-01-27 12:30:17 +0530

    Aggregation Rev

--- 5-mongo-notes-n-projects/mongoDB code/mongoose_node/index.js
+++ 5-mongo-notes-n-projects/mongoDB code/mongoose_node/index.js
@@ -22,19 +22,6 @@ const productSchema = new mongoose.Schema({
 const Product = new mongoose.model("Product", productSchema);
 
 
-// 💖 Thank You So Much For Choosing My Video 💖
-
-// Hi everyone,
-
-// I'm absolutely thrilled – we're almost at 600K subscribers for our MongoDB course! This course was a true labor of love, and it's been amazing to see how it's helping you all.
-
-// If you've enjoyed what we're doing and want to be part of our journey, hitting that Subscribe button would mean the world to me. Let's keep growing and learning together! 
-// Here is the link: https://www.youtube.com/thapatechnical
-
-// With gratitude,
-// Thapa Technical 
-
-
 
 //? 2nd step while inserting the data
 const data1 = {
@@ -88,13 +75,3 @@ db.Students.insertMany(
     age: 21,
   }]
 );
-
-
- {
-    "name": "Binamra",
-    "age": 20,
-  }
-  {
-    "name": "Thapa",
-    "age": 21,
-  }
